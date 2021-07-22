1d5e81b0ac229ecf7268bb11eb85bbfec39ed7a7
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-07-22 19:52:19 -0400

    Remove some unneeded newlines.

--- lib/steam/steam.go
+++ lib/steam/steam.go
@@ -14,16 +14,13 @@ var STEAM_APPS_ROOT = ".steam/steam/steamapps"
 func LibraryFolders() (directories []string, err error) {
 	// get the users HOME
 	home, err := os.UserHomeDir()
-
 	if err != nil {
 		return
 	}
 
 	// create the 'libraryfolders.vdf' path and parse it
 	vdf_path := fmt.Sprintf("%s/%s/%s", home, STEAM_APPS_ROOT, "libraryfolders.vdf")
-
 	kv, err := steamvdf.ReadFile(vdf_path)
-
 	if err != nil {
 		return
 	}
@@ -38,7 +35,6 @@ func LibraryFolders() (directories []string, err error) {
 
 		if err == nil {
 			child, _ := kv.GetChild(key)
-
 			directories = append(directories, child.Value)
 		}
 	}
