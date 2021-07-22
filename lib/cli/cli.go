7781327551ac3aa3f74f6e2a8947876e5b940f00
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-07-21 21:13:16 -0400

    Remove unused functions and help var.

--- lib/cli/cli.go
+++ lib/cli/cli.go
@@ -8,28 +8,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var help = `toshokan
-Tool for searching installed Steam games for their APPID.
-
-USAGE:
-	toshokan [OPTIONS] <SEARCH>...
-
-OPTIONS:
-	-h, --help
-		Show this help screen.
-
-ARGS:
-	<SEARCH>	Search terms for game name(s).
-`
-
-func validateArgs() bool {
-	return len(os.Args) >= 2
-}
-
-func getSearchTerms() string {
-	return strings.Join(os.Args[1:], " ")
-}
-
 func preRunHelp(cmd *cobra.Command, args []string) error {
 	if len(args) == 0 {
 		cmd.Help()
