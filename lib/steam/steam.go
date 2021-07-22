f81aed1504302275094b222576797ba2c22ef105
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-07-22 16:45:34 -0400

    Remove Test function. Pass errors to return.

--- lib/steam/steam.go
+++ lib/steam/steam.go
@@ -1,16 +1,22 @@
 package steam
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Jleagle/steam-go/steamvdf"
+)
 
 var STEAM_APPS_ROOT = ".steam/steam/steamapps"
 var STEAM_LIBRARY_FOLDERS_VDF = fmt.Sprintf("%s/libraryfolders.vdf", STEAM_APPS_ROOT)
 
-func getLibraryFolders() []string {
-	return []string{"hello"}
-}
+func GetLibraryFolders() ([]string, error) {
+	kv, err := steamvdf.ReadFile(STEAM_LIBRARY_FOLDERS_VDF)
 
-func Test() {
-	folders := getLibraryFolders()
+	if err != nil {
+		return nil, fmt.Errorf("unable to read: %s", STEAM_LIBRARY_FOLDERS_VDF)
+	}
 
-	fmt.Println(folders)
+	fmt.Println(kv)
+
+	return []string{kv.Key}, nil
 }
