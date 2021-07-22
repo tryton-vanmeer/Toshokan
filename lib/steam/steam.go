d09ea9e8aa65d589484e28b8e36052bf8f2f18e3
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-07-22 17:13:55 -0400

    Add default steam library to folders array.

--- lib/steam/steam.go
+++ lib/steam/steam.go
@@ -29,7 +29,10 @@ func GetLibraryFolders() []string {
 		log.Fatalf("cannot read: %s", vdf_path)
 	}
 
-	folders := []string{}
+	// create arrary to append libraries to, with default library path as first element
+	folders := []string{
+		fmt.Sprintf("%s/%s", home, STEAM_APPS_ROOT),
+	}
 
 	// if the key is an integer, it maps to a steam library
 	for key := range kv.GetChildrenAsMap() {
