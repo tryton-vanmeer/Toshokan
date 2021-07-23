ffeb4b92ce346b1da5a0c7a7cc47a3295f2ea460
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-07-22 20:36:11 -0400

    Use log.Fatalf instead of returning error since not a shared lib.

--- lib/steam/steam.go
+++ lib/steam/steam.go
@@ -2,6 +2,7 @@ package steam
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -11,18 +12,18 @@ import (
 var STEAM_APPS_ROOT = ".steam/steam/steamapps"
 
 // get the users configured steam libraries
-func LibraryFolders() (directories []string, err error) {
+func LibraryFolders() (directories []string) {
 	// get the users HOME
 	home, err := os.UserHomeDir()
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	// create the 'libraryfolders.vdf' path and parse it
 	vdf_path := fmt.Sprintf("%s/%s/%s", home, STEAM_APPS_ROOT, "libraryfolders.vdf")
 	kv, err := steamvdf.ReadFile(vdf_path)
 	if err != nil {
-		return
+		log.Fatalf("error reading %s", vdf_path)
 	}
 
 	// add the default library
@@ -40,4 +41,8 @@ func LibraryFolders() (directories []string, err error) {
 	}
 
 	return
+}
+
+func FindAppManifests(directory string) {
+
 }
