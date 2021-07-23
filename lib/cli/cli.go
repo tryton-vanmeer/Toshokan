27944fed18605ecc19afce59a32183c12d06749f
Tryton Van Meer <hello@trytonvanmeer.dev>
2021-07-23 06:04:32 -0400

    Split search command out from the root command.

--- lib/cli/cli.go
+++ lib/cli/cli.go
@@ -2,41 +2,32 @@ package cli
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"toshokan/lib/steam"
 
 	"github.com/spf13/cobra"
 )
 
-func preRunHelp(cmd *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		cmd.Help()
-		os.Exit(0)
-	}
+var searchCmd = &cobra.Command{
+	Use:     "search [flags] ARGS...",
+	Args:    cobra.MinimumNArgs(1),
+	Short:   "Search installed Steam games to find their APPID",
+	Example: "toshokan half life",
+	Run: func(cmd *cobra.Command, args []string) {
+		games := steam.SearchInstalledGames(strings.Join(args, " "))
 
-	return nil
+		for _, game := range games {
+			fmt.Printf("%s (%s) [%s]\n", game.Name, game.AppID, game.LibraryFolder)
+		}
+	},
 }
 
-func searchCmd(cmd *cobra.Command, args []string) {
-	games := steam.SearchInstalledGames(strings.Join(args, " "))
-
-	for _, game := range games {
-		fmt.Printf("%s (%s) [%s]\n", game.Name, game.AppID, game.LibraryFolder)
-	}
-}
-
-func Run() error {
+func Run() {
 	var rootCmd = &cobra.Command{
-		Use:     "toshokan [flags] SEARCH...",
-		Short:   "Search installed Steam games to find their APPID.",
-		Long:    "Toshokan is a CLI tool for searching your installed Steam games to find their APPID.",
-		Example: "toshokan half life",
-		PreRunE: preRunHelp,
-		Run:     searchCmd,
+		Long: "Toshokan is a CLI tool for searching your installed Steam games to find their APPID.",
 	}
+
+	rootCmd.AddCommand(searchCmd)
 
 	rootCmd.Execute()
-
-	return nil
 }
