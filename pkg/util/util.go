f39f29c0ad8f5b8258d5c1d3664eeed492258140
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-02-14 18:32:39 -0500

    remove hyperlink functions.

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -11,12 +10,4 @@ func StringContains(s string, substr string) bool {
 	substr = strings.ToLower(substr)
 
 	return strings.Contains(s, substr)
-}
-
-func FileHyperlink(file string) string {
-	return Hyperlink("file://"+file, file)
-}
-
-func Hyperlink(url string, text string) string {
-	return fmt.Sprintf("\033]8;;%s\033\\%s\033]8;;\033\\", url, text)
 }
