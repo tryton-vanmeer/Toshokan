41405eb22bec44970cb7bcd4cc6b4c431f7ce0ed
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-03-17 17:09:54 -0400

    add basic TUI with bubbletea libs.

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/Jleagle/steam-go v0.0.0-20210211214415-6e5c1aecbba1
+	github.com/charmbracelet/bubbles v0.10.3
+	github.com/charmbracelet/bubbletea v0.20.0
 	github.com/spf13/cobra v1.2.1
 )
--- pkg/tui/tui.go
+++ pkg/tui/tui.go
@@ -1,7 +1,75 @@
 package tui
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"toshokan/pkg/steam"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type item steam.App
+
+func (i item) Title() string       { return i.Name }
+func (i item) FilterValue() string { return i.Name }
+func (i item) Description() string { return i.AppID }
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
 
 func Run() {
-	fmt.Println("Hello from TUI")
+	items := []list.Item{}
+	games := steam.InstalledGames()
+
+	for _, game := range games {
+		item := item{
+			Name:             game.Name,
+			AppID:            game.AppID,
+			LibraryFolder:    game.LibraryFolder,
+			InstallDirectory: game.InstallDirectory,
+		}
+
+		items = append(items, item)
+	}
+
+	m := model{list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "Toshokan"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if err := p.Start(); err != nil {
+		fmt.Println("error running program:", err)
+		os.Exit(1)
+	}
 }
