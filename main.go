1fc04dca3ad2394a83de20ecc58ea1ce0c5a7b5f
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-03-21 18:52:18 -0400

    simple promptui impl.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"toshokan/pkg/steam"
 
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+	games := steam.GetApps()
+	games.Sort()
+
+	prompt := promptui.Select{
+		Label: "Steam Games",
+		Items: games,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	fmt.Println(games[i])
 }
