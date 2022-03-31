97dc1e8bf26307ea58fa5db7f085e9d71d1ee8d9
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-03-31 13:57:16 -0400

    move promptui stuff to tui package.

--- main.go
+++ main.go
@@ -1,44 +1,9 @@
 package main
 
 import (
-	"strings"
-	"toshokan/pkg/steam"
-
-	"github.com/manifoldco/promptui"
+	"toshokan/pkg/tui"
 )
 
 func main() {
-	games := steam.GetApps()
-	games.Sort()
-
-	details := `
-{{ "App ID:" | faint }} {{ .AppID }}
-{{ "Store Page:" | faint }} {{ .GetStorePage }}
-{{ "Install Directory:" | faint }} {{ .InstallDirectory }}
-{{ if .ProtonPrefix }}{{ "Proton Prefix:" | faint }} {{ .ProtonPrefix }}{{ end }}`
-
-	templates := &promptui.SelectTemplates{
-		Label:    "{{ . }}",
-		Active:   "{{ .Name | underline }}",
-		Inactive: "{{ .Name }}",
-		Details:  details,
-	}
-
-	searcher := func(input string, index int) bool {
-		game := games[index]
-		name := strings.ToLower(game.Name)
-		input = strings.ToLower(input)
-
-		return strings.Contains(name, input)
-	}
-
-	prompt := promptui.Select{
-		Label:        "Games",
-		HideSelected: true,
-		Items:        games,
-		Templates:    templates,
-		Searcher:     searcher,
-	}
-
-	prompt.Run()
+	tui.Run()
 }
