5bc897e76bee2c9915bdf37537a1cd01efa93e10
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-04-30 14:15:30 -0400

    add prettytable for output.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+#[macro_use] extern crate prettytable;
+use prettytable::{Table, Row, Cell, Attr, format};
+
 mod steam;
 
 fn main() {
     let games = steam::get_games();
 
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_CLEAN);
+
+    table.set_titles(
+        Row::new(vec![
+            Cell::new("App ID")
+            .with_style(Attr::Bold),
+            Cell::new("Name")
+            .with_style(Attr::Bold)
+        ])
+    );
+
     for game in games {
-        println!("{}  {}", game.name, game.appid)
+        table.add_row(row![game.appid, game.name]);
     }
+
+    table.printstd();
 }
