16d8dee27876e91c17162cad8e0524839a2708d0
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-04-30 19:19:59 -0400

    set theme colours.

--- src/tui.rs
+++ src/tui.rs
@@ -1,13 +1,20 @@
 use crate::steam;
 use crate::steam::Game;
 
-use cursive::{views::*, Cursive, theme::*, traits::{Scrollable, Resizable}};
+use cursive::{views::*, Cursive, theme::*, theme::{PaletteColor::*, Color::*, BaseColor::*}, traits::{Scrollable}};
 
 fn set_theme(siv: &mut Cursive) {
     let mut theme = siv.current_theme().clone();
+    let mut palette = Palette::default();
+
+    palette[Background] = TerminalDefault;
+    palette[View] = TerminalDefault;
+    palette[Primary] = TerminalDefault;
+    palette[Secondary] = TerminalDefault;
 
     theme.shadow = false;
     theme.borders = BorderStyle::Simple;
+    theme.palette = palette;
 
     siv.set_theme(theme);
 }
