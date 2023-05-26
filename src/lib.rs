964dceb661a55bd5e3538e2df5980e0ace5a1bc7
Tryton Van Meer <code@trytonvanmeer.dev>
2023-05-26 13:06:13 -0400

    add Game struct built from steamlocate apps.

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use std::env;
+
+use anyhow::{Context, Ok, Result};
+use steamlocate::{SteamApp, SteamDir};
+
+pub struct Game {
+    pub appid: u32,
+    pub name: String,
+    path: String,
+}
+
+impl Game {
+    fn from_steamapp(id: &u32, app: &SteamApp) -> Self {
+        Self {
+            appid: *id,
+            name: app.name.as_ref().unwrap().clone(),
+            path: app.path.display().to_string(),
+        }
+    }
+
+    fn path(&self) -> String {
+        let home = env::var("HOME").unwrap();
+
+        if self.path.starts_with(&home) {
+            return self.path.replace(&home, "~");
+        }
+
+        self.path.clone()
+    }
+}
+
+pub fn get_games() -> Result<Vec<Game>> {
+    let mut steam = SteamDir::locate().context("unable to find steamdir")?;
+
+    let mut games: Vec<Game> = steam
+        .apps()
+        .iter()
+        .map(|(id, app)| Game::from_steamapp(id, app.as_ref().unwrap()))
+        .collect::<Vec<Game>>();
+
+    games.sort_by_key(|game| game.name.to_string());
+
+    Ok(games)
+}
