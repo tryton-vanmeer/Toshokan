679fa24be31be3ebfdaf78e4f2222035213061f3
Tryton Van Meer <code@trytonvanmeer.dev>
2023-05-25 23:01:35 -0400

    add list command with stub function.

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,30 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(author, version)]
-/// Tool for interacting with your Steam library.
-struct Args {}
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// List installed games in your Steam library
+    List,
+}
+
+fn list() -> Result<()> {
+    println!("{}", toshokan::get_games());
+    Ok(())
+}
 
 pub fn run() -> Result<()> {
-    let args = Args::parse();
+    let args = Cli::parse();
+
+    match args.cmd {
+        Command::List => list()?
+    }
 
     Ok(())
 }
