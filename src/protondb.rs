a11e0f3c3296c06e976b70f32b1ac5c5ebd0be09
Tryton Van Meer <code@trytonvanmeer.dev>
2023-05-27 17:07:54 -0400

    request protondb api as part of init func of Summery.

--- src/protondb.rs
+++ src/protondb.rs
@@ -3,25 +3,27 @@ use reqwest::{blocking::Client, header::USER_AGENT};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct Summary {
+pub struct Summary {
     tier: String,
 }
 
-pub fn get_tier(appid: String) -> Result<String> {
-    let url = format!(
-        "https://www.protondb.com/api/v1/reports/summaries/{}.json",
-        appid
-    );
+impl Summary {
+    pub fn from_appid(appid: String) -> Result<Self> {
+        let url = format!(
+            "https://www.protondb.com/api/v1/reports/summaries/{}.json",
+            appid
+        );
 
-    let response = Client::new()
-        .get(url)
-        .header(
-            USER_AGENT,
-            format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")),
-        )
-        .send()?;
+        let response = Client::new()
+            .get(url)
+            .header(
+                USER_AGENT,
+                format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")),
+            )
+            .send()?;
 
-    let summary: Summary = serde_json::from_str(&response.text()?)?;
+        let summary: Summary = serde_json::from_str(&response.text()?)?;
 
-    Ok(summary.tier)
+        Ok(summary)
+    }
 }
