a8e31d870474360e705ba3ef5299cef7ad91db72
Tryton Van Meer <code@trytonvanmeer.dev>
2024-02-16 21:50:28 -0500

    feat: update and support latest verson of steamlocate

--- src/steam.rs
+++ src/steam.rs
@@ -1,7 +1,7 @@
 use std::{env, path::PathBuf};
 
-use anyhow::{Context, Ok, Result};
-use steamlocate::{SteamApp, SteamDir};
+use anyhow::{Context, Result};
+use steamlocate::{App, Library, SteamDir};
 
 trait DisplayHomeAsTilde {
     fn display_home_as_tilde(&self) -> String;
@@ -29,33 +29,18 @@ pub struct Game {
 }
 
 impl Game {
-    fn from_steamapp(id: &u32, app: &SteamApp) -> Self {
+    fn from_steamapp(app: &App, library: &Library) -> Self {
         let proton = match app.user_config.get("platform_override_source") {
             Some(platform) => platform == "windows",
             _ => false,
         };
 
         Self {
-            appid: *id,
-            name: app.name.as_ref().unwrap().clone(),
+            appid: app.app_id,
+            name: app.name.clone().unwrap(),
             proton,
-            path: app.path.clone(),
+            path: library.resolve_app_dir(app),
         }
-    }
-
-    fn should_filter(&self) -> bool {
-        ![
-            "Proton 6.3",
-            "Proton 7.0",
-            "Proton 8.0",
-            "Proton EasyAntiCheat Runtime",
-            "Proton Experimental",
-            "Proton Hotfix",
-            "Steamworks Common Redistributables",
-            "Steam Linux Runtime - Sniper",
-            "Steam Linux Runtime - Soldier",
-        ]
-        .contains(&self.name.as_ref())
     }
 
     pub fn path(&self) -> String {
@@ -74,15 +59,43 @@ impl Game {
     }
 }
 
-pub fn get_games() -> Result<Vec<Game>> {
-    let mut steam = SteamDir::locate().context("unable to find steamdir")?;
+fn should_filter(name: &str) -> bool {
+    ![
+        "Proton 4.11",
+        "Proton 6.3",
+        "Proton 7.0",
+        "Proton 8.0",
+        "Proton EasyAntiCheat Runtime",
+        "Proton Experimental",
+        "Proton Hotfix",
+        "Steamworks Common Redistributables",
+        "Steam Linux Runtime - Sniper",
+        "Steam Linux Runtime - Soldier",
+    ]
+    .contains(&name)
+}
 
-    let mut games: Vec<Game> = steam
-        .apps()
-        .iter()
-        .map(|(id, app)| Game::from_steamapp(id, app.as_ref().unwrap()))
-        .filter(|game| game.should_filter())
-        .collect::<Vec<Game>>();
+pub fn get_games() -> Result<Vec<Game>> {
+    let steam = SteamDir::locate().context("unable to find steamdir")?;
+    let mut games: Vec<Game> = Vec::new();
+
+    for library in steam.libraries()? {
+        match library {
+            Err(err) => eprintln!("failed reading library: {err}"),
+            Ok(library) => {
+                for app in library.apps() {
+                    match app {
+                        Err(err) => eprintln!("failed reading app: {err}"),
+                        Ok(app) => {
+                            if should_filter(app.name.as_ref().unwrap()){
+                                games.push(Game::from_steamapp(&app, &library))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     games.sort_by_key(|game| game.name.to_string());
 
@@ -90,12 +103,10 @@ pub fn get_games() -> Result<Vec<Game>> {
 }
 
 pub fn get_game(appid: u32) -> Result<Game> {
-    let mut steam = SteamDir::locate().context("unable to find steamdir")?;
+    let steam = SteamDir::locate().context("unable to find steamdir")?;
+    let (app, library) = steam
+        .find_app(appid)?
+        .context(format!("unable to find game with appid {}", appid))?;
 
-    Ok(Game::from_steamapp(
-        &appid,
-        steam
-            .app(&appid)
-            .context(format!("unable to find game with appid {}", appid))?,
-    ))
+    Ok(Game::from_steamapp(&app, &library))
 }
